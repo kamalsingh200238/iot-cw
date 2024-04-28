33b9cad9e09805fa116652660dcfeb156c2b2631
Kamal Singh <kamalsingh200238@gmail.com>
2024-04-28 14:25:26 +0100

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./*.html", "./src/**/*.{js,ts,html}"],
+  content: ["./*.{js,html}"],
   theme: {
     extend: {
       borderWidth: {
